85d040f8963ee6c4eac81727ec751d845792e97c
Milo Rastgoo <hi@informagician.com>
2020-02-28 11:29:47 -0500

    login process completed, jwt pending

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,6 +20,22 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  if(req.body.username && req.body.password){
+    Auth.login(req.body)
+    .then(user => {
+      if(user && bcrypt.compareSync(req.body.password, user.password)){
+        res.status(200).json({token:'Success'})
+      } else {
+        res.status(401).json({message:'Invalid Username and Password'})
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({message:'Something went wrong during Login'})
+    })
+  } else {
+    res.status(400).json({message:'Username and Password Required'})
+  }
 });
 
 module.exports = router;
