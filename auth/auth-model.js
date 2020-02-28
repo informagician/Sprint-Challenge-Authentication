397a068ca667ef5ba7fbb41bd3739273d171a938
Milo Rastgoo <hi@informagician.com>
2020-02-28 11:19:08 -0500

    auth model created, user registration working

---
+++ auth/auth-model.js
@@ -0,0 +1,19 @@
+const knex = require('knex')
+const db = knex(require('../knexfile').development)
+
+module.exports = {
+    register,
+    login
+}
+
+function register(user) {
+    return db('users')
+    .insert(user,'id')
+}
+
+function login(user) {
+    return db('users')
+    .select('*')
+    .where({username:user.username})
+    .first()
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const Auth = require('./auth-model')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  if(req.body.username && req.body.password){
+    Auth.register(req.body)
+    .then(id => {
+      res.status(201).json({message:`user with id ${id} created.`})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({message:'something went wrong during user registration'})
+    })
+  } else {
+    res.status(400).json('Bad Request')
+  }
 });
 
 router.post('/login', (req, res) => {
