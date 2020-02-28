71c28fea321b8b9250ea59dfbecf0435b58c0be6
Milo Rastgoo <hi@informagician.com>
2020-02-28 11:43:11 -0500

    middleware completed and implemented in jokes router

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,22 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken')
+
+const secret = 'abcdefghijklmnopqrst1234567890'
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  if(req.headers.authorization){
+    jwt.verify(req.headers.authorization, secret, (err,decodedToken) => {
+      if(err){
+        res.status(401).json({ message: 'Invalid Authorization' });
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else {
+    res.status(400).json({ message: 'No Authorization Provided' });
+  }
 };
