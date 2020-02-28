c27a6e21812d1dc8d8fcf05ce500dddc10349f67
Milo Rastgoo <hi@informagician.com>
2020-02-28 11:57:38 -0500

    react app installed

---
+++ auth/auth.spec.js
@@ -0,0 +1,13 @@
+//const request = require('supertest')
+
+//const server = require('../api/server')
+
+describe('Auth Router', function(){
+    it('should run tests', function(){
+        expect(true).toBe(true)
+    })
+
+    // describe('POST /register', function(){
+    //     it('')
+    // })
+})
